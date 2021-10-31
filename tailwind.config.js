cdea82e93e3000833bf8b4ac94cda567f57c25e7
a2036913 <a2036913@hotmail.com>
2021-10-31 17:10:56 +0800

    finish setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#202225',
+        secondary: '#5865f2',
+        gray: colors.trueGray,
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5',
+        }
+      }
+    },
   },
   variants: {
     extend: {},
